eff89b61f69927ad67d1a2bb22d7b60a4c0ea5ba
VounThida <vounthida1406@gmail.com>
2024-10-08 14:37:59 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// // tailwind.config.js
+// module.exports = {
+//   future: {},
+//   purge: [],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         body: ['Nunito']
+//       }
+//     },
+//   },
+//   variants: {},
+//   plugins: [],
+// }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
